53b8e6fafcc2d2dc68aae3800976a8f5dd49ea18
Matt Stiles <mattstiles@gmail.com>
2021-03-20 14:45:16 -0700

    Update download_json.sh

--- download_json.sh
+++ download_json.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 NOW=$(date +"%m-%d-%Y")
 
-BOUNDARIES="https://services3.arcgis.com/uknczv4rpevve42E/ArcGIS/rest/services/Reopening_Status_to_Districts_FGDB/FeatureServer/0/"
+BOUNDARIES="https://services3.arcgis.com/T4QMspbfLg3qTGWY/ArcGIS/rest/services/Public_Wildfire_Perimeters_View/FeatureServer/0"
 
 QUERY="query?f=json&returnGeometry=true&outFields=*&where=1=1"
 
